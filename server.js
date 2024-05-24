c85be4fb8064e467edbb921a5dece68a54c974eb
nolevit1 <nolevit1@students.zhaw.ch>
2024-05-24 22:39:52 +0200

    alle files und im server.js eine erweiterung

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict';
+const express = require('express');
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+// App
+const app = express();
+app.get('/', (req, res) => {
+res.send('Hello FS2024 DevOps Course! :-) I am a Node.js app running in a Docker container.');
+});
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
